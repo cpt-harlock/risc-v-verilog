fe0c99dd054cdaab06e8280392193eb0dd5c15cd
harlock <monterubbiano@di.uniroma1.it>
2022-04-26 16:36:44 +0200

    implemented stall signal in cu; implemented stall in dec pipe; must implement forwarding

--- cu.sv
+++ cu.sv
@@ -1,71 +1,115 @@
 `include "defines.vh"
 `include "opcodes.vh"
 
-module cu (
-    input [6:0] opcode_fetch,
-    input [6:0] opcode_dec,
-    input [6:0] opcode_ex,
-    input [2:0] funct_3_ex,
-    input funct_7_bit_ex,
-    input [6:0] opcode_mem,
-    // branch taken signal computed by ex phase
-    input branch_taken, //branch taken signal logic implemented in EX unit ...
-    // signal to nop pipelines
-    output nop_output_fetch,
-    output nop_output_dec,
-    output nop_output_ex,
-    output nop_output_mem,
-    output [1:0] fetch_sel,
-    // signal for exec pipeline reg
-    output store_mem,
-    output load_mem,
-    output store_reg,
-    output [2:0] size,
-    output sign
-);
+module cu (input [6:0] opcode_fetch,
+           input [6:0] opcode_dec,
+           input [6:0] opcode_ex,
+           input [2:0] funct_3_ex,
+           input funct_7_bit_ex,
+           input [6:0] opcode_mem,
+           input branch_taken,       //branch taken signal logic implemented in EX unit ...
+           input [4:0] rd_ex,
+           input [4:0] rs1_dec,
+           input [4:0] rs2_dec,
+           output nop_output_fetch,
+           output nop_output_dec,
+           output nop_output_ex,
+           output nop_output_mem,
+           output [1:0] fetch_sel,
+           output store_mem,
+           output load_mem,
+           output store_reg,
+           output [2:0] size,
+           output sign,
+           output stall,
+           output forward_mem_ex_rs1,
+           output forward_mem_ex_rs2,
+           output forward_wb_ex_rs1,
+           output forward_wb_ex_rs2
+           );
+    
+    function [2:0] get_instruction_type;
+        input [6:0] opcode;
+        case (opcode)
+            7'b0000011,
+            7'b0010011,
+            7'b1100111: get_instruction_type = `I;
+            7'b0100011: get_instruction_type = `S;
+            7'b0010111,
+            7'b0110111: get_instruction_type = `U;
+            7'b1100011: get_instruction_type = `B;
+            7'b1101111: get_instruction_type = `J;
+            7'b0110011: get_instruction_type = `R;
+            default: get_instruction_type    = `WRONG_INSTRUCTION_TYPE;
+        endcase
+    endfunction
+    
+    function write_to_register_instruction;
+        input [6:0] opcode;
+        case (opcode)
+            7'b0000011,
+            7'b0010011,
+            7'b1100111: write_to_register_instruction = 1;
+            7'b0100011: write_to_register_instruction = 0;
+            7'b0010111,
+            7'b0110111: write_to_register_instruction = 1;
+            7'b1100011: write_to_register_instruction = 0;
+            7'b1101111: write_to_register_instruction = 1;
+            7'b0110011: write_to_register_instruction = 1;
+            default: write_to_register_instruction    = 0;
+        endcase
+    endfunction
 
-function [2:0] get_instruction_type;
-    input [6:0] opcode;
-    case (opcode)
-        7'b0000011,
-        7'b0010011,
-        7'b1100111: get_instruction_type = `I; 
-        7'b0100011: get_instruction_type = `S;
-        7'b0010111,
-        7'b0110111: get_instruction_type = `U;
-        7'b1100011: get_instruction_type = `B;
-        7'b1101111: get_instruction_type = `J;
-        7'b0110011: get_instruction_type = `R;
-        default: get_instruction_type = `WRONG_INSTRUCTION_TYPE;
-    endcase
-endfunction
 
-// insert 2 bubbles behind branch instruction 
-assign nop_output_fetch = get_instruction_type(opcode_fetch) == `B || get_instruction_type(opcode_fetch) == `J || opcode_fetch == 7'b1100111 ? 1 :
-                          get_instruction_type(opcode_dec) == `B || get_instruction_type(opcode_dec) == `J || opcode_dec == 7'b1100111 ? 1 :
-                          0;
-// fetch selector is old pc while branch doesn't reach EX unit
-// NOTE: we can avoid stopping the PC for jump instruction since we're going to load it with new value in any case
-assign fetch_sel = get_instruction_type(opcode_fetch) == `B ? `FETCH_SEL_NOP :
-                   get_instruction_type(opcode_dec) == `B  ? `FETCH_SEL_NOP :
-                   branch_taken == 1 ? `FETCH_SEL_BRANCH :
-                   `FETCH_SEL_PC;
+    function read_from_register_instruction;
+        input [6:0] opcode;
+        case (opcode)
+            7'b0000011,
+            7'b0010011,
+            7'b1100111: read_from_register_instruction = 1;
+            7'b0100011: read_from_register_instruction = 1;
+            7'b0010111,
+            7'b0110111: read_from_register_instruction = 1;
+            7'b1100011: read_from_register_instruction = 1;
+            7'b1101111: read_from_register_instruction = 0;
+            7'b0110011: read_from_register_instruction = 1;
+            default: read_from_register_instruction    = 1;
+        endcase
+    endfunction
+    // insert 2 bubbles behind branch instruction
+    assign nop_output_fetch = get_instruction_type(opcode_fetch) == `B || get_instruction_type(opcode_fetch) == `J || opcode_fetch == 7'b1100111 ? 1 :
+    get_instruction_type(opcode_dec) == `B || get_instruction_type(opcode_dec) == `J || opcode_dec == 7'b1100111 ? 1 :
+    0;
+    // fetch selector is old pc while branch doesn't reach EX unit
+    // NOTE: we can avoid stopping the PC for jump instruction since we're going to load it with new value in any case
+    assign fetch_sel = branch_taken == 1 ? `FETCH_SEL_BRANCH : stall == 1 ? `FETCH_SEL_NOP : 
+    get_instruction_type(opcode_dec) == `B  ? `FETCH_SEL_NOP :
+    get_instruction_type(opcode_fetch) == `B ? `FETCH_SEL_NOP :
+    `FETCH_SEL_PC;
+    
+    assign store_mem = get_instruction_type(opcode_ex) == `S ? 1 : 0;
+    assign load_mem = {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LB ? 1 :
+    {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LH ? 1 :
+    {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LW ? 1 :
+    {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LBU ? 1 :
+    {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LHU ? 1 :
+    0;
+    assign store_reg = get_instruction_type(opcode_mem) == `R ? 1 :
+    get_instruction_type(opcode_mem) == `I ? 1 :
+    get_instruction_type(opcode_mem) == `J ? 1 :
+    get_instruction_type(opcode_mem) == `U ? 1 : 0;
+    
+    assign size = {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LB || {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `SB || {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LBU ? 1 :
+    {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LH || {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `SH || {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LHU ? 2 :
+    {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LW || {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `SW ? 4 : 0;
+    
+    assign sign = {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LB || {opcode_ex, funct_3_ex, funct_7_bit_ex} == ? `LH ? 1 : 0;
+    
+    //stall should be like a temporary asynchronous reset for dec pipeline...
+    // for fetch, we can read again the instruction since we're stalling the PC
+    assign stall = write_to_register_instruction(opcode_ex) && 
+    read_from_register_instruction(opcode_dec) && (rd_ex == rs1_dec || rd_ex == rs2_dec);
+    
+    assign nop_output_dec = stall;
 
-assign store_mem = get_instruction_type(opcode_ex) == `S ? 1 : 0;
-assign load_mem = {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LB ? 1 :
-                    {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LH ? 1 :
-                    {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LW ? 1 :
-                    {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LBU ? 1 :
-                    {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LHU ? 1 :
-                    0;
-assign store_reg = get_instruction_type(opcode_mem) == `R ? 1 :
-                    get_instruction_type(opcode_mem) == `I ? 1 :
-                    get_instruction_type(opcode_mem) == `J ? 1 :
-                    get_instruction_type(opcode_mem) == `U ? 1 : 0;
-
-assign size = {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LB || {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `SB || {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LBU ? 1 :
-                {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LH || {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `SH || {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LHU ? 2 :
-                {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LW || {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `SW ? 4 : 0;
-
-assign sign = {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LB || {opcode_ex, funct_3_ex, funct_7_bit_ex} ==? `LH ? 1 : 0;
 endmodule
--- pipeline_dec.sv
+++ pipeline_dec.sv
@@ -10,33 +10,61 @@ module pipeline_dec (input clk,
                      input [31:0] rs1_data_in,
                      input [31:0] rs2_data_in,
                      input [31:0] pc_in,
-                     output reg [6:0] opcode_out,
-                     output reg [4:0] rd_out,
-                     output reg [2:0] funct3_out,
-                     output reg [4:0] rs1_out,
-                     output reg [4:0] rs2_out,
-                     output reg [6:0] funct7_out,
-                     output reg [31:0] imm_out,
-                     output reg [31:0] rs1_data_out,
-                     output reg [31:0] rs2_data_out,
-                     output reg [31:0] pc_out);
+                     input stall,
+                     output [6:0] opcode_out,
+                     output [4:0] rd_out,
+                     output [2:0] funct3_out,
+                     output [4:0] rs1_out,
+                     output [4:0] rs2_out,
+                     output [6:0] funct7_out,
+                     output [31:0] imm_out,
+                     output [31:0] rs1_data_out,
+                     output [31:0] rs2_data_out,
+                     output [31:0] pc_out);
     
-    always @(negedge clk) begin
-        if (nop_output == 1)
-        begin
-        end
-        else begin
-            opcode_out <= opcode_in;
-            rd_out <= rd_in;
-            funct3_out <= funct3_in;
-            rs1_out <= rs1_in;
-            rs2_out <= rs2_in;
-            funct7_out <= funct7_in;
-            imm_out <= imm_in;
-            rs1_data_out <= rs1_data_in;
-            rs2_data_out <= rs2_data_in;
-            pc_out <= pc_in;
+    reg [6:0] opcode_out_temp;
+    reg [4:0] rd_out_temp;
+    reg [2:0] funct3_out_temp;
+    reg [4:0] rs1_out_temp;
+    reg [4:0] rs2_out_temp;
+    reg [6:0] funct7_out_temp;
+    reg [31:0] imm_out_temp;
+    reg [31:0] rs1_data_out_temp;
+    reg [31:0] rs2_data_out_temp;
+    reg [31:0] pc_out_temp;
+    
+    
+    always @(negedge clk, stall) begin
+        if (!stall) begin
+            if (nop_output == 1)
+            begin
+                //TODO: implement!!!
+            end
+            else begin
+                opcode_out_temp   <= opcode_in;
+                rd_out_temp       <= rd_in;
+                funct3_out_temp   <= funct3_in;
+                rs1_out_temp      <= rs1_in;
+                rs2_out_temp      <= rs2_in;
+                funct7_out_temp   <= funct7_in;
+                imm_out_temp      <= imm_in;
+                rs1_data_out_temp <= rs1_data_in;
+                rs2_data_out_temp <= rs2_data_in;
+                pc_out_temp       <= pc_in;
+            end
         end
     end
+
+    assign opcode_out == !stall ? opcode_out_temp : `NOP_INSTRUCTION_OPCODE;    
+    assign rd_out == !stall ? rd_out_temp : `NOP_INSTRUCTION_RD;    
+    assign funct3_out == !stall ? funct3_out_temp : `NOP_INSTRUCTION_FUNCT3;    
+    assign funct7_out == !stall ? funct7_out_temp : `NOP_INSTRUCTION_FUNCT7;    
+    assign rs1_out == !stall ? rs1_out_temp : `NOP_INSTRUCTION_RS1;    
+    assign rs2_out == !stall ? rs2_out_temp : `NOP_INSTRUCTION_RS2;    
+    assign imm_out == !stall ? imm_out_temp : `NOP_INSTRUCTION_IMM;    
+    assign rs1_data_out == !stall ? rs1_data_out_temp : `NOP_INSTRUCTION_RS1_DATA;    
+    assign rs2_data_out == !stall ? rs2_data_out_temp : `NOP_INSTRUCTION_RS2_DATA;    
+    assign pc_out == !stall ? pc_out_temp : `NOP_INSTRUCTION_PC;    
     
+
 endmodule
